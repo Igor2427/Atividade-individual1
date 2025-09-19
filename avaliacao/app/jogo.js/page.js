f63ed37445114a77ee70c47084326e272833dd5f
Igor2427 <128043201+Igor2427@users.noreply.github.com>
2025-09-19 17:55:54 -0300

    Update page.js

--- avaliacao/app/jogo.js/page.js
+++ avaliacao/app/jogo.js/page.js
@@ -1,27 +1,5 @@
-import Link from "next/link";
-import JogoForca from "../../components/JogoForca";
+import JogoForca from "@/components/JogoForca";
 
-export default function Jogo() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <Link href="/">
-        <button style={{
-          backgroundColor: "#1e3a8a",
-          color: "white",
-          padding: "10px 20px",
-          border: "none",
-          borderRadius: "8px",
-          marginBottom: "20px",
-          cursor: "pointer"
-        }}>
-          Voltar para o Portfólio
-        </button>
-      </Link>
-
-      <h1 style={{ textAlign: "center" }}>Jogo da Forca</h1>
-
-      {/* Aqui o jogo é renderizado */}
-      <JogoForca />
-    </div>
-  );
+export default function JogoPage() {
+  return <JogoForca />;
 }
